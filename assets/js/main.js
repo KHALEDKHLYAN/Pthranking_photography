89070ac62908927d6d8bd0dedd79a431e91fc230
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2024-05-28 09:47:50 +0300

    lazzy loading

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,11 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
    */
   const preloader = document.querySelector('#preloader');
   if (preloader) {
-    window.addEventListener('load', () => {
+    const removePreloader = () => {
       preloader.classList.add('loaded');
       setTimeout(() => {
         preloader.remove();
       }, 1000);
+    };
+
+    // Remove preloader after maximum 10 seconds
+    const preloaderTimeout = setTimeout(removePreloader, 10000);
+
+    window.addEventListener('load', () => {
+      clearTimeout(preloaderTimeout);
+      removePreloader();
     });
   }
 
